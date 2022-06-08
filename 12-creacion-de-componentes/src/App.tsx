83aaad81247390101eec3e62772301516e67984b
kike <kike@kikes-MacBook-Pro.local>
2022-06-08 18:40:17 -0500

    DONE->12-creacion-de-componentes

---
+++ 12-creacion-de-componentes/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import {HolaMundo} from "./componentes/HolaMundo";
+
+function App() {
+  return (
+  <>
+    <HolaMundo />
+  </>
+  );
+}
+
+export default App;
---
+++ 12-creacion-de-componentes/src/componentes/HolaMundo.tsx
@@ -0,0 +1,10 @@
+interface HolaMundoProps {
+
+}
+
+export const HolaMundo: React.FC<HolaMundoProps> = ({}) => {
+
+    return(<>
+        Hola Mundo desde un componente
+        </>)
+}
