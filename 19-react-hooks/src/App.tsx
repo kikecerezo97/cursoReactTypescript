86ba8eb915dc537b5da7fe0d9f62956c443f6607
kike <kike@kikes-MacBook-Pro.local>
2022-10-01 23:22:21 -0500

    DONE->react-hooks-usestate

---
+++ 19-react-hooks/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import {Contador} from "./contador/Contador";
+
+function App() {
+  return (
+    <>
+      <Contador/>
+    </>
+  );
+}
+
+export default App;
